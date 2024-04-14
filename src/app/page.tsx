00abd53b735a0ed1b248f7667b8b2956ec3a2795
viss5vuus98 <dokipu@gmailcom>
2024-04-14 17:37:29 +0800

    Update set path aliases

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,16 @@
-import React from 'react';
+import React from 'react'
 
 export default function Home() {
   return (
-    <main className='min-h-screen'>
-      <div className=''>
-        <h1 className="text-3xl font-bold text-center mt-10">Welcome to my Next.js App!</h1>
-        <p className="text-lg text-center mt-4">This is a simple example page.</p>
+    <main className="min-h-screen">
+      <div className="">
+        <h1 className="mt-10 text-center text-3xl font-bold">
+          Welcome to my Next.js App!
+        </h1>
+        <p className="mt-4 flex text-center text-lg">
+          This is a simple example page.
+        </p>
       </div>
     </main>
-  );
+  )
 }
