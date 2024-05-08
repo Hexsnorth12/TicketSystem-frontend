86c84c8c3e21bedc761e8dad9f1dd373fa51b646
viss5vuus98 <dokipu@gmailcom>
2024-05-08 23:32:02 +0800

    Refactor use server action for authentication due to middleware cache limitation 1. Add lib folder with dal.ts to handle authentication 2. Modify middleware to only decode base64

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,35 +1,19 @@
 import { NextRequest, NextResponse } from 'next/server'
 import { cookies } from 'next/headers'
 import { BASE_URL, serverCode, protectedRoutes } from '@/definitions'
+import { getSession } from './lib'
 
 export async function middleware(req: NextRequest) {
-    const token = cookies().get('token')?.value
-    const response = await fetch(`${BASE_URL}api/v1/user`, {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json',
-            Authorization: `Bearer ${token}`,
-        },
-        cache: 'force-cache',
-    }).then((res) => res.json())
-
-    const { status, data: profile } = response
+    const session = getSession()
     const path = req.nextUrl.pathname
     const isProtectedRoute = protectedRoutes.includes(path)
 
-    // 取不到 user info 一律重導回登入頁
-    if (isProtectedRoute && status === serverCode.NOT_LOGGEN_IN) {
+    // 樂觀驗證, 只還原 base64 取 user id , 取不到session 重導回登入頁
+    if (isProtectedRoute && !session?.id) {
         return NextResponse.redirect(new URL('/login', req.url))
     }
 
-    const next_response = NextResponse.next()
-    next_response.cookies.set({
-        name: 'user-profile',
-        value: JSON.stringify(profile),
-        httpOnly: true,
-        path: '/',
-    })
-    return next_response
+    return NextResponse.next()
 }
 
 // match all except specific paths
