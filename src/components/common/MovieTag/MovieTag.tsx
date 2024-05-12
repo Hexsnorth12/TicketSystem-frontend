b44b3a722d4e5fc27f6df3c73f29f54486532f29
viss5vuus98 <dokipu@gmailcom>
2024-05-12 19:32:39 +0800

    fix build error

--- src/components/common/MovieTag/MovieTag.tsx
+++ src/components/common/MovieTag/MovieTag.tsx
@@ -6,7 +6,7 @@ interface MovieTagProps {
 
 const MovieTag: React.FC<MovieTagProps> = ({ label }) => {
     return (
-        <div className="rounded-primary inline-block bg-gray-1 px-3 py-1">
+        <div className="inline-block rounded-primary bg-gray-1 px-3 py-1">
             <span className="text-small2 text-primary">{label}</span>
         </div>
     )
