29ad991bc94640d6eb10cbaed2eb2e596ef02a09
Johnny Hsu <a209328641@gmail.com>
2024-05-22 22:55:47 +0800

    fix: change TypeTag from Button to div

--- src/components/Buttons/TypeTag.tsx
+++ src/components/Buttons/TypeTag.tsx
@@ -1,15 +1,13 @@
 import React from 'react'
-import { Button } from '../common'
 
 import type { TypeTag as TypeTagProps } from '@/types'
 
 export const TypeTag: React.FC<TypeTagProps> = ({ tagName }) => {
     return (
-        <Button
-            type="button"
+        <div
             title={`tag - ${tagName}`}
             className="rounded border-0 bg-gray-3 px-3 py-1 text-sm">
             {tagName}
-        </Button>
+        </div>
     )
 }
