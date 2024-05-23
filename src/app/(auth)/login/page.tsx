ea067875171a0b8e6c81b8cca433097c35a0bc6f
viss5vuus98 <dokipu@gmailcom>
2024-05-23 21:35:36 +0800

    fix build error

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,10 +1,12 @@
-import React from 'react'
+import React, { Suspense } from 'react'
 import { SignIn } from '@components/forms'
 
 const Page = () => {
     return (
         <div className="mx-auto p-4">
-            <SignIn />
+            <Suspense>
+                <SignIn />
+            </Suspense>
         </div>
     )
 }
