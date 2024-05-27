753ad7018fb98415ea09e73c8111fdbca5609070
Wen Yun Chiang <jennychiang2016@gmail.com>
2024-05-27 12:46:58 +0800

    RWD vesion

--- src/definitions/movieDetailData.ts
+++ src/definitions/movieDetailData.ts
@@ -1,7 +1,7 @@
 export const MovieDetailCards = [
     {
         name: '沙丘二',
-        image: '/assets/popcard3.jpg',
+        image: '/assets/popcard1.jpg',
         type: '動作片',
         rank: 3.8,
         price: 800,
