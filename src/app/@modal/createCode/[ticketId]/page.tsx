9fc424fdfc7db37afffe8d3793e3693b70bdcceb
viss5vuus98 <dokipu@gmailcom>
2024-06-02 12:14:27 +0800

    fix build error

--- src/app/@modal/createCode/[ticketId]/page.tsx
+++ src/app/@modal/createCode/[ticketId]/page.tsx
@@ -11,9 +11,7 @@ const Page: React.FC<pageProps> = () => {
             <ModalContent tittle="取得分票碼" hasCancel>
                 <div className="flex p-4">
                     <div className="rounded-l-lg border border-r-0 border-gray-3 bg-gray-1 px-3 py-2">
-                        <p className="text-small1 text-white">
-                            3141232131231
-                        </p>
+                        <p className="text-small1 text-white">3141232131231</p>
                     </div>
                     <div className="items-center rounded-r-lg border border-gray-3 bg-gray-1 px-3 py-2 text-primary hover:bg-primary hover:text-gray-1">
                         <span className="text-small2 md:text-small1">複製</span>
--- src/components/member/ShareTicket/ShareTicket.stories.tsx
+++ src/components/member/ShareTicket/ShareTicket.stories.tsx
@@ -3,21 +3,21 @@ import type { Meta, StoryObj } from '@storybook/react'
 import ShareTicket from './ShareTicket'
 
 const meta = {
-  title: 'ticket/ShareTicket',
-  component: ShareTicket,
-  parameters: {
-      nextjs: {
-          appDirectory: true,
-      },
-  },
-  decorators: (story) => (
-      <div className="container h-lvh bg-black">{story()}</div>
-  ),
+    title: 'ticket/ShareTicket',
+    component: ShareTicket,
+    parameters: {
+        nextjs: {
+            appDirectory: true,
+        },
+    },
+    decorators: (story) => (
+        <div className="container h-lvh bg-black">{story()}</div>
+    ),
 } satisfies Meta<typeof ShareTicket>
 
 export default meta
 type Story = StoryObj<typeof meta>
 
 export const Default: Story = {
-  args: {},
+    args: {},
 }
