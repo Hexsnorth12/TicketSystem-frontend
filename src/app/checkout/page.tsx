b3ffe1ac1428ef3533588afe006a1635fd28e4a3
Wen Yun Chiang <jennychiang2016@gmail.com>
2024-06-04 18:50:09 +0800

    checkout page v1

--- src/app/checkout/page.tsx
+++ src/app/checkout/page.tsx
@@ -1,10 +1,9 @@
-'use client'
 import React from 'react'
-import { useRouter } from 'next/navigation'
-import CartTable from '@components/common/Table/cartTable'
+import CheckoutTable from '@components/common/Table/checkoutTable'
 import { Button } from '@/components/common/index'
+import { Delivery } from '@/components/forms'
 
-const CartPage = () => {
+const CheckoutPage = () => {
     interface Column {
         title: string
         dataIndex: keyof DataSource
@@ -71,47 +70,38 @@ const CartPage = () => {
             price: 200,
         },
     ]
-    const router = useRouter()
-    const handleCheckout = () => {
-        router.push('/checkout')
-    }
+
     return (
         <div className="relative isolate overflow-hidden py-16 sm:py-16">
             <div className="mx-auto max-w-7xl px-6 lg:px-6">
                 <div className="mx-auto max-w-2xl lg:mx-0">
                     <h2 className="text-4xl font-bold tracking-wider text-white sm:text-6xl">
-                        購物車
+                        配送資料
                     </h2>
                 </div>
                 <div className="mx-auto mt-10 max-w-7xl lg:mx-0 lg:max-w-none">
-                    <div className="flex flex-col flex-wrap items-start justify-around gap-x-4 text-left md:flex-row md:flex-nowrap">
-                        <div className=" md:basis-2/3">
-                            <CartTable
+                    <div className="flex flex-col  flex-wrap items-start justify-around gap-x-4 text-left  md:flex-nowrap">
+                        <div className="w-full">
+                            <CheckoutTable
                                 columns={columns}
                                 dataSource={dataSource}
                             />
                         </div>
-                        <div className="mt-3 w-full rounded-lg bg-gray-3 p-4 text-white md:mt-0 md:basis-1/3 ">
-                            <div className="flex items-center gap-x-4">
+                        <div className="mt-3 w-full  rounded-lg bg-gray-3 p-4  text-white md:mt-0 ">
+                            {/* <div className="flex justify-end gap-x-4">
                                 <h4 className="flex-none text-body font-semibold leading-6 text-white md:text-3xl">
                                     結算
                                 </h4>
-                            </div>
+                            </div> */}
                             <ul
                                 role="list"
-                                className="mb-4 mt-10 grid grid-cols-1 gap-4  text-small1 leading-6  text-white sm:grid-cols-1 sm:gap-6 md:text-body">
+                                className="mb-4 mt-10 grid grid-cols-1 justify-items-end gap-4  text-small1 leading-6  text-white sm:grid-cols-1 sm:gap-6 md:text-body">
                                 <li className="flex gap-x-3">金額:350 NT</li>
                                 <li className="flex gap-x-3">折扣：-35 NT</li>
                                 <li className="flex gap-x-3">總金額:315 NT</li>
                                 <div className="h-px flex-auto bg-gray-100" />
                             </ul>
-                            <Button
-                                onClick={handleCheckout}
-                                type="button"
-                                title=""
-                                className="w-full">
-                                去買單
-                            </Button>
+                            <Delivery />
                         </div>
                     </div>
                 </div>
@@ -120,4 +110,4 @@ const CartPage = () => {
     )
 }
 
-export default CartPage
+export default CheckoutPage
