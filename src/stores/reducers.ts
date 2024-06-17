ec67cfaeeb269a5a502f8e45ccb8d826bba400b6
Wen Yun Chiang <jennychiang2016@gmail.com>
2024-06-17 15:23:09 +0800

    fix

--- src/stores/reducers.ts
+++ src/stores/reducers.ts
@@ -1,4 +1,4 @@
-import { combineReducers } from '@reduxjs/toolkit'
+import { combineReducers } from 'redux'
 import userSlice from './slices/userSlice'
 import { api, nextApi } from '@/services/apiSlice'
 
