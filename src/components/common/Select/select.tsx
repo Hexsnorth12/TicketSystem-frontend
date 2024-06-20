eebd18315d54bccee98f0ae087ee2d28818b88ad
viss5vuus98 <dokipu@gmailcom>
2024-06-20 22:12:06 +0800

    Fix select

--- src/components/common/Select/select.tsx
+++ src/components/common/Select/select.tsx
@@ -34,26 +34,28 @@ const SelectInput: React.FC<SelectProps> = ({
             value={selectedValue}
             onValueChange={(newValue) => handleChange(newValue ?? '')}>
             <Select.Trigger
-                className={clsx(
-                    'hover:bg-mauve3 border-1 ring-black-300/10 inline-flex h-full w-full items-center justify-between rounded-md border-gray-3 bg-gray-1 px-4 py-3 text-small2 leading-150 text-white shadow-sm ring-1 placeholder:text-small2 placeholder:text-gray-4 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-primary md:text-body md:placeholder:text-body',
-                    className, // Apply custom className
-                )}
+                className="text-violet11 hover:bg-mauve3 data-[placeholder]:text-violet9 inline-flex w-full items-center justify-between gap-[5px] rounded border border-gray-3 bg-gray-1 px-4 py-3 text-body leading-none text-white shadow-[0_2px_10px] shadow-black/10 outline-none focus:shadow-[0_0_0_2px] focus:shadow-primary w-f"
                 aria-label="city"
                 disabled={options.length === 0}
-                asChild={false}>
+                asChild={false}
+                data-state={'open'}>
                 <Select.Value placeholder={placeholder} />
                 <Select.Icon className="text-violet11">
                     <ChevronDownIcon />
                 </Select.Icon>
             </Select.Trigger>
             <Select.Portal>
-                <Select.Content className="z-50 w-full overflow-hidden rounded-md bg-white shadow-[0px_10px_38px_-10px_rgba(22,_23,_24,_0.35),0px_10px_20px_-15px_rgba(22,_23,_24,_0.2)]">
+                <Select.Content
+                    className="z-50 mt-1 overflow-hidden rounded-md bg-gray-1 text-white shadow-[0px_10px_38px_-10px_rgba(22,_23,_24,_0.35),0px_10px_20px_-15px_rgba(22,_23,_24,_0.2)]"
+                    position="popper"
+                    style={{ width: 'var(--radix-select-trigger-width)' }}
+                    sideOffset={5}>
                     <Select.ScrollUpButton className="text-violet11 flex h-[25px] cursor-default items-center justify-center bg-white">
                         <ChevronUpIcon />
                     </Select.ScrollUpButton>
                     <Select.Viewport className="rounded-lg border border-gray-3">
                         <Select.Group>
-                            <Select.Label className="flex justify-start text-body">
+                            <Select.Label className="flex items-center justify-start px-4 py-3 text-body">
                                 {label}
                             </Select.Label>
                             {options.map((option, index) => (
@@ -89,17 +91,11 @@ const SelectItem: React.FC<SelectItemProps> = ({
     ...props
 }) => {
     return (
-        <Select.Item
-            value={value}
-            className={
-                'text-violet11 data-[disabled]:text-mauve8 data-[highlighted]:bg-violet9 data-[highlighted]:text-violet1 relative flex h-[25px] select-none items-center rounded-[3px] pl-[25px] pr-[35px] text-[13px] leading-none data-[disabled]:pointer-events-none data-[highlighted]:outline-none'
-            }
-            onClick={onClick}
-            {...props}>
+        <Select.Item value={value} onClick={onClick} {...props}>
             <Select.ItemText>{children}</Select.ItemText>
-            <Select.ItemIndicator className="absolute left-0 inline-flex w-[25px] items-center justify-center">
+            {/* <Select.ItemIndicator className="absolute left-0 inline-flex w-[25px] items-center justify-center">
                 <CheckIcon />
-            </Select.ItemIndicator>
+            </Select.ItemIndicator> */}
         </Select.Item>
     )
 }
