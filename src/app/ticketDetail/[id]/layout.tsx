100bbd750d9ca35386fe1c74268d8c7eb8ca23f4
viss5vuus98 <dokipu@gmailcom>
2024-06-29 12:46:44 +0800

    Fix user ticket detail

--- src/app/ticketDetail/[id]/layout.tsx
+++ src/app/ticketDetail/[id]/layout.tsx
@@ -5,7 +5,10 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
     return (
         <>
             <div className="container hidden md:my-10 md:block">
-                <PrevPage pageName="我的電影票" pagePath="/tickets" />
+                <PrevPage
+                    pageName="我的電影票"
+                    pagePath="/user/tickets?status=unverified"
+                />
             </div>
 
             <div className="container mt-6 flex h-screen md:mt-0">
