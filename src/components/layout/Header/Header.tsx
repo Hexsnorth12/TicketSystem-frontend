323084b6ad4cc289280fa2cc7b11f0557f3a2558
jou.hsu <slackbrrDEV289!>
2024-07-04 13:50:53 +0800

    Update toggle navbar method

--- src/components/layout/Header/Header.tsx
+++ src/components/layout/Header/Header.tsx
@@ -47,9 +47,9 @@ const Header: React.FC<HeaderProps> = ({ logoSrc }) => {
             }
         }
 
-        document.addEventListener('mousedown', handleClickOutside)
+        document.addEventListener('click', handleClickOutside)
         return () => {
-            document.removeEventListener('mousedown', handleClickOutside)
+            document.removeEventListener('click', handleClickOutside)
         }
     }, [])
 
